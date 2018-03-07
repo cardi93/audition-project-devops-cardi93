37065b9d6fa45458dec8ace5a7ff0dd8fc3e62e7
cardi93 <36967655+cardi93@users.noreply.github.com>
2018-03-07 16:28:54 +0100

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -6,5 +6,5 @@ int main()
 	std::vector<double> vector = {  };
 	std::cout << "Media: " << LibTwoUtils::sample_mean(vector) << std::endl;
 	std::cout << "Massimo: " << LibTwoUtils::find_max(vector) << std::endl;
-	return LibTwoUtils::sample_mean(vector) || LibTwoUtils::find_max(vector);
+	return !LibTwoUtils::sample_mean(vector) || !LibTwoUtils::find_max(vector);
 }
