8a64b5e1c9f5e34cb80d62805b0ec5d3978841e2
cardi93 <36967655+cardi93@users.noreply.github.com>
2018-03-07 16:41:29 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,5 +6,9 @@ int main()
 	std::vector<double> vector = { 2 };
 	std::cout << "Media: " << LibTwoUtils::sample_mean(vector) << std::endl;
 	std::cout << "Massimo: " << LibTwoUtils::find_max(vector) << std::endl;
-	return !LibTwoUtils::sample_mean(vector) || !LibTwoUtils::find_max(vector);
+	if (LibTwoUtils::sample_mean(vector) == 0)
+		if (LibTwoUtils::find_max(vector) != 0)
+			return 0;
+		else return 1;
+	else return 1;
 }
