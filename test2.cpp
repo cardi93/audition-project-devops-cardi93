507e1a544e54bd89bfb4ee3220a63b5c0ac32e19
cardi93 <36967655+cardi93@users.noreply.github.com>
2018-03-08 21:43:51 +0100

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-	std::vector<double> vector = { -8, 7.5, 5.6 , -4 };
+	std::vector<double> vector = { -8, -5, 6 , 11 };
 	std::cout << "Media: " << LibTwoUtils::sample_mean(vector) << std::endl;
 	std::cout << "Massimo: " << LibTwoUtils::find_max(vector) << std::endl;
-	if (LibTwoUtils::sample_mean(vector) == 0.275)
-		if (LibTwoUtils::find_max(vector) == 7.5)
+	if (LibTwoUtils::sample_mean(vector) == 1)
+		if (LibTwoUtils::find_max(vector) == 11)
 			return 0;
 		else return 1;
 	else return 1;
